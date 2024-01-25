f7d5e6bf1124ab9f408f8b35d64dfc5119104018
PDgaming <produnyadehingia12345@gmail.com>
2024-01-25 13:44:41 +0000

    implemented gemini pro

--- src/routes/grade-app.ai/+page.svelte
+++ src/routes/grade-app.ai/+page.svelte
@@ -1,16 +1,43 @@
 <script>
     import "./chatWindowStyle.css";
-
-    // Function to send a message and handle bot response
-    async function sendMessage() {
+    import { GoogleGenerativeAI } from "@google/generative-ai";
+    
+    let userInputs = [];
+    let GeminiResponses = [];
+    function sendMessage() {
         const userInput = document.getElementById("userInput").value.trim();
-        console.log(userInput);
+        userInputs = [...userInputs, userInput];
+        document.getElementById("userInput").value = "";
+        run(userInput);
+    }
+
+    const API_KEY="AIzaSyD3pylacar54cPdqHQDPc1bWHBEU_-97uE";
+
+    const genAI = new GoogleGenerativeAI(API_KEY);
+
+    async function run(prompt) {
+        const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+        const result = await model.generateContent(prompt);
+        const response = await result.response;
+        const text = response.text();
+        GeminiResponses = [...GeminiResponses, text];
     }
 </script>
 
 <div class="main">
     <div class="title">
         <text style="position: relative; top: 8px;">Grade App</text>
+    </div>
+    <div class="chat">
+        <p>
+            {#each userInputs as userInput}
+                {userInput}
+            {/each}<br>
+            {#each GeminiResponses as GeminiResponse}
+                {GeminiResponse}
+            {/each}<br>
+        </p>
     </div>
     <div class="typebox">
         <input type="text" id="userInput" placeholder="Type your question..." />
