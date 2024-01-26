57558e0f7384d526ed7afcd2ac8c4345cf39a5cf
PD <89239979+PDgaming@users.noreply.github.com>
2024-01-26 10:49:59 +0530

    added responsive media queries in index.css
    Now the home page is responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Navbar from "./components/navbar.svelte";
 	import "./index.css";
 	import { goto } from "$app/navigation";
-	import laptop from "./images/laptop.png"
+	import laptop from "./images/laptop.png";
 
 	function loginPage() {
 		goto("/login");
@@ -18,8 +18,9 @@
 </svelte:head>
 
 <div class="main">
+	<h1 class="brandName">Grade App</h1>
 	<Navbar />
-	<img class="laptopImg" src={laptop} alt="laptop.img">
+	<img class="laptopImg" src={laptop} alt="laptop.img" />
 	<div class="home" id="home">
 		<h7>Grade App - Educational App</h7>
 		<p>
