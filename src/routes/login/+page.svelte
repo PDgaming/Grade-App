81420f83c5350afd4d14fca64e404e60458de36c
PD <89239979+PDgaming@users.noreply.github.com>
2024-02-04 09:25:10 +0530

    Update +page.svelte

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,8 +3,6 @@
     import { goto } from "$app/navigation";
     import { initializeApp } from "firebase/app";
     import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
-    // https://firebase.google.com/docs/web/setup#available-libraries
-    // Your web app's Firebase configuration
     const firebaseConfig = {
         apiKey: "AIzaSyB_MSh9YlBu7GGN5wxZjY7kGN4bU697GO4",
 
