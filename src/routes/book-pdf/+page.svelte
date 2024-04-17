a0a3bb64196da9cc44cedc5ff3285ae729d73201
PD <Produnyadehingia12345@gmail.com>
2024-04-17 18:42:05 +0530

    Added a few new things
    Added, dashboard, book-pdf, premium. These are still empty pages and do not have much interactivity.

---
+++ src/routes/book-pdf/+page.svelte
@@ -0,0 +1,11 @@
+<svelte:head>
+  <title>Grade App - Book PDFs</title>
+</svelte:head>
+
+<h1>Sorry, but this feature not ready yet</h1>
+
+<style>
+  h1 {
+    color: white;
+  }
+</style>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,6 +1,10 @@
 <script></script>
 
+<svelte:head>
+  <title>Grade App - Dashboard</title>
+</svelte:head>
 <h1>Hello There, welcome To Your Dashboard!</h1>
+<h2>So what do you want to do today?</h2>
 <a href="/premium-login">Grade App</a>
 <a href="/book-pdf">Books</a>
 
@@ -9,6 +13,9 @@
     color: white;
     margin-top: 50px;
     font-weight: bold;
+  }
+  h2 {
+    color: white;
   }
   a {
     color: white;
