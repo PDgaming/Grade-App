f853f8f8c154adc771689aef129713eb9e485493
PD <Produnyadehingia12345@gmail.com>
2024-04-18 13:10:39 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,22 @@
 import adapter from "@sveltejs/adapter-cloudflare";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+  },
+
+  preprocess: vitePreprocess(),
+
+  vitePlugin: {
+    experimental: {
+      renderBuiltUrl: "https://grade-app.pages.dev/", // Replace with your website URL
+    },
+  },
+
+  package: {
+    files: (file) => file === "static/sw.js",
   },
 };
 
