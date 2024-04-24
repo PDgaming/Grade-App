afcbd9900f92d24b6092c1a38714afd54019299a
PD <Produnyadehingia12345@gmail.com>
2024-04-24 19:51:41 +0530

    Added framework for adding more books in the future

--- src/routes/book-pdf/ncert-pdf-data.js
+++ src/routes/book-pdf/ncert-pdf-data.js
@@ -1,32 +1,71 @@
 const ncertData = {
   classes: {
-    class_VI: {},
-    class_VII: {},
-    class_VIII: {
+    class_VI: {
       subject: {
-        English: {
-          book: {
-            Honeydew: {
-              chapters: {
-                chapter_1: "https://ncert.nic.in/textbook/pdf/hehd101.pdf",
-                chapter_2: "https://ncert.nic.in/textbook/pdf/hehd102.pdf",
-                chapter_3: "https://ncert.nic.in/textbook/pdf/hehd103.pdf",
-                chapter_4: "https://ncert.nic.in/textbook/pdf/hehd104.pdf",
-                chapter_5: "https://ncert.nic.in/textbook/pdf/hehd105.pdf",
-                chapter_6: "https://ncert.nic.in/textbook/pdf/hehd106.pdf",
-                chapter_7: "https://ncert.nic.in/textbook/pdf/hehd107.pdf",
-                chapter_8: "https://ncert.nic.in/textbook/pdf/hehd108.pdf",
-                download: "https://ncert.nic.in/textbook/pdf/hehd1dd.zip",
-              },
-            },
-            It_so_happened: {},
-          },
-        },
+        Hindi: {},
+        English: {},
         Mathematics: {},
+        Social_Science: {},
+        Science: {},
       },
     },
-    class_IX: {},
-    class_X: {},
+  },
+  class_VII: {
+    subject: {
+      English: {},
+      Mathematics: {},
+      Hindi: {},
+      Science: {},
+      Social_Science: {},
+    },
+  },
+  class_VIII: {
+    subject: {
+      English: {
+        book: {
+          Honeydew: {
+            chapters: {
+              chapter_1: "https://ncert.nic.in/textbook/pdf/hehd101.pdf",
+              chapter_2: "https://ncert.nic.in/textbook/pdf/hehd102.pdf",
+              chapter_3: "https://ncert.nic.in/textbook/pdf/hehd103.pdf",
+              chapter_4: "https://ncert.nic.in/textbook/pdf/hehd104.pdf",
+              chapter_5: "https://ncert.nic.in/textbook/pdf/hehd105.pdf",
+              chapter_6: "https://ncert.nic.in/textbook/pdf/hehd106.pdf",
+              chapter_7: "https://ncert.nic.in/textbook/pdf/hehd107.pdf",
+              chapter_8: "https://ncert.nic.in/textbook/pdf/hehd108.pdf",
+              download: "https://ncert.nic.in/textbook/pdf/hehd1dd.zip",
+            },
+          },
+          It_so_happened: {},
+        },
+      },
+      Mathematics: {},
+      Hindi: {},
+      Science: {},
+      Social_Science: {},
+    },
+  },
+  class_IX: {
+    subject: {
+      English: {
+        book: {},
+      },
+      Hindi: {},
+      Mathematics: {},
+      Science: {},
+      Social_Science: {},
+    },
+  },
+  class_X: {
+    subject: {
+      Mathematics: {},
+      Science: {},
+      Hindi: {},
+      Social_Science: {},
+      English: {
+        book: {},
+      },
+    },
   },
 };
 
