936ec56d3aa511ee6a06366bfab5b3fcfd82ba6f
PD <89239979+PDgaming@users.noreply.github.com>
2024-05-11 18:02:33 +0530

    Update +page.svelte

---
+++ src/routes/notes/+page.svelte
@@ -0,0 +1,21 @@
+<script>    
+    import { initializeApp } from "firebase/app";
+    import { getStorage } from "firebase/storage";
+
+    // config for firebase
+    const firebaseConfig = {
+        apiKey: "AIzaSyB_MSh9YlBu7GGN5wxZjY7kGN4bU697GO4",
+        
+        authDomain: "grade-app-16e2d.firebaseapp.com",
+        
+        projectId: "grade-app-16e2d",
+        
+        storageBucket: "grade-app-16e2d.appspot.com",
+        
+        messagingSenderId: "942886540823",
+        
+        appId: "1:942886540823:web:29caeac2695fecc3d4ee52",
+        
+        measurementId: "G-WD1M20G6LX",
+        };
+</script>
