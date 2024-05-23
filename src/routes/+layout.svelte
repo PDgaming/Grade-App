770f9dfdfdb7e804745c18dcb2e19583daae20c4
PD <89239979+PDgaming@users.noreply.github.com>
2024-05-23 14:29:16 +0530

    Added a new feature. now http request status codes are translated into dog images using http dogs status

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,25 @@
   import "./index.css";
   import "bootstrap/dist/css/bootstrap.min.css";
   import { registerServiceWorker } from "../registerServiceWorker";
+  import { afterNavigate } from "$app/navigation";
 
   registerServiceWorker();
+
+  let statusCode;
+  async function getStatus() {
+    try {
+      const myResponse = await fetch(window.location.href);
+      statusCode = myResponse.status;
+      if (statusCode !== 200) {
+        window.location.href = "https://http.dog/" + statusCode + ".jpg";
+      }
+    } catch (err) {
+      console.log("There was an error:", err);
+    }
+  }
+  afterNavigate(() => {
+    getStatus();
+  });
 </script>
 
 <div class="app">
