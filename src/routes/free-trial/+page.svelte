b4771485f9a0d0c38a8f6f553813aafeaa441cc4
PD <89239979+PDgaming@users.noreply.github.com>
2024-06-08 12:07:50 +0530

    updated api keys

--- src/routes/free-trial/+page.svelte
+++ src/routes/free-trial/+page.svelte
@@ -67,7 +67,7 @@
     }
   }
 
-  const API_KEY = "AIzaSyD5t-e0VhB5tKMwTwZ102FYh2NQcFR624A"; // API key
+  const API_KEY = "AIzaSyBXn4fdb8a7ej5Ln53cJwtsadgGWjHF5v0"; // API key
 
   const genAI = new GoogleGenerativeAI(API_KEY); // generates a new ai to using the api key to get responses
 
