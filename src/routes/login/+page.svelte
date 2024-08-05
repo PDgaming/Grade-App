ab38e4d0a928da79e12d7fa7ceaf4d78454e321f
PD <89239979+PDgaming@users.noreply.github.com>
2024-08-05 15:05:59 +0530

    updated the login page with a more modern look

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -6,7 +6,6 @@
   import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
   import { UsersDatabase } from "../supabaseClient";
   import { toasts, ToastContainer, FlatToast } from "svelte-toasts";
-  import LoginForm from "../components/loginForm.svelte";
 
   // config for firebase
   const firebaseConfig = {
@@ -157,74 +156,76 @@
   <h1>Login</h1>
 </center>
 <center id="normal">
-  <div class="container">
-    <div class="oAuthLoginButtons">
-      <button
-        id="loginWithGoogleButton"
-        class="btn btn-primary"
-        on:click={loginWithGoogle}
-        ><svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 488 512"
-          id="google-icon"
-          ><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
-            d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-          /></svg
-        ></button
-      >
-    </div>
-    <hr style="margin-bottom: 30px;" />
-    <LoginForm />
-    <button id="loginButton" class="btn btn-primary" on:click={loginWIthEmail}
-      >Login</button
-    >
-
-    <br />
-    <span id="register">
-      <a href="/register">Register</a>
-    </span>
+  <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mt-4">
+    <center>
+      <div class="oAuthLoginButtons">
+        <button
+          id="loginWithGoogleButton"
+          class="btn btn-primary"
+          on:click={loginWithGoogle}
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 488 512"
+            id="google-icon"
+            ><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
+              d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+            /></svg
+          ></button
+        >
+      </div>
+      <form class="card-body" style="color: white">
+        <label class="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            class="h-4 w-4 opacity-70"
+          >
+            <path
+              d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z"
+            />
+            <path
+              d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z"
+            />
+          </svg>
+          <input type="text" class="grow" placeholder="Email" id="email" />
+        </label>
+        <label class="input input-bordered flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            class="h-4 w-4 opacity-70"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <input
+            type="password"
+            class="grow"
+            placeholder="Password"
+            id="password"
+          />
+        </label>
+        <button
+          id="loginButton"
+          class="btn btn-primary"
+          on:click={loginWIthEmail}>Login</button
+        >
+      </form>
+      <span id="register">
+        <a href="/register">Register</a>
+      </span>
+    </center>
   </div>
-</center>
-
-<center id="responsive">
-  <div class="oAuthLoginButtons" style="margin-top: 40px; margin-bottom: 30px;">
-    <button
-      id="loginWithGoogleButton"
-      class="btn btn-primary"
-      on:click={loginWithGoogle}
-      ><svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 488 512"
-        id="google-icon"
-        ><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path
-          d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-        /></svg
-      ></button
-    >
-  </div>
-  <LoginForm />
-  <button id="loginButton" class="btn btn-primary" on:click={loginWIthEmail}
-    >Login</button
-  >
-
-  <br />
-  <span id="register">
-    <a href="/register">Register</a>
-  </span>
 </center>
 
 <style>
   #login-text {
     margin-top: 30px;
-  }
-  .container {
-    background-color: #2c2f34;
-    margin-top: 40px;
-    border-radius: 10px;
-    width: 35vw;
-    height: auto;
-    padding-bottom: 10px;
-    text-align: center;
   }
   h1 {
     font-size: 55px;
@@ -234,12 +235,8 @@
     gap: 10px;
     margin-top: 10px;
   }
-  #loginWithGoogleButton {
-    font-size: 20px;
-  }
   #loginButton {
     font-size: 25px;
-    width: 120px;
     margin-bottom: 10px;
   }
   #register a {
@@ -251,13 +248,5 @@
   }
   #google-icon {
     width: 30px;
-  }
-  @media (max-width: 1000px) {
-    #normal {
-      display: none;
-    }
-    #responsive {
-      display: block;
-    }
   }
 </style>
